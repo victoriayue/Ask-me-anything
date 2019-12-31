0966b6d55aa4a9158d76e34e2d413b30455242bd
Yian Yue <victoriayue05@gmail.com>
2019-12-31 12:32:29 -0700

    Initial Commit

---
+++ Ask me anything/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Ask me anything
+//
+//  Created by Yi an Yue on 12/31/19.
+//  Copyright © 2019 Yi an Yue. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
